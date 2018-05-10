4abdd9668b5c036d3ba1c51aa9b7767ddf06b301
Mike del Castillo <johnmichaeldc@gmail.com>
2018-05-10 16:57:54 +0800

    it works now

--- index.js
+++ index.js
@@ -32,11 +32,11 @@ function windowLoad(){
 }
 
 function update(){
-  //ctx.fillStyle = "rgba(" + 0x00 + ", " + 0x55 + ", " + 0x8C + ", 0.05)";
-  //ctx.fillStyle = "rgba(" + 0x00 + ", " + 0x0 + ", " + 0x0 + ", 0.1)";
-  //ctx.fillStyle = "rgba(" + Math.floor(100 ) + "," + Math.floor(0) + "," + Math.floor(100) + ", 0.5)";
+  // ctx.fillStyle = "rgba(" + 0x00 + ", " + 0x55 + ", " + 0x8C + ", 0.05)";
+  ctx.fillStyle = "rgba(" + 0x00 + ", " + 0x0 + ", " + 0x0 + ", 0.1)";
+  // ctx.fillStyle = "rgba(" + Math.floor(100 ) + "," + Math.floor(0) + "," + Math.floor(100) + ", 0.5)";
 
-  //ctx.fillRect(0, 0, c.width, c.height);
+  ctx.fillRect(0, 0, c.width, c.height);
 
   windowPosition.vx = (window.screenX - windowPosition.x);
   windowPosition.vy = (window.screenY - windowPosition.y);
@@ -46,7 +46,7 @@ function update(){
 
   for(var i in balls){
     balls[i].update();
-    //balls[i].draw();
+    balls[i].draw();
   }
 
   window.requestAnimationFrame(update);
