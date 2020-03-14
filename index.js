59f9120cc75962af11d175734a43178b038b8fa1
mikedelcastillo <johnmichaeldc@gmail.com>
2020-03-15 00:31:20 +0800

    Rewrote the code to be faster and be easier to read

--- index.js
+++ index.js
@@ -1,91 +1,264 @@
-var c, ctx,
-    mx = my = mux = muy = 0, md = false,
-    PI = Math.PI, TAU = PI * 2,
-    keyCodes = [];
+const canvas = document.querySelector("#canvas")
+const context = canvas.getContext("2d")
 
-function windowLoad(){
-  c = document.getElementById("canvas");
-  ctx = c.getContext("2d");
-
-  //initAudio();
-
-  windowResize();
-  window.addEventListener("resize", windowResize, false);
-
-  c.addEventListener("mousemove", mouseMove, false);
-  c.addEventListener("mousedown", mouseDown, false);
-  c.addEventListener("mouseup", mouseUp, false);
-
-  document.addEventListener("keydown", keyDown, false);
-  document.addEventListener("keyup", keyUp, false);
-
-  windowPosition.x = window.screenX + 0;
-  windowPosition.y = window.screenY + 0;
-
-  n = Number(prompt("how many balls"));
-
-  for(var i = 0; i < n; i++){
-    new Ball(Math.random() * c.width, Math.random() * c.height, Math.random() * 90 + 10);
-  }
-
-  update();
+let balls = []
+let chunks = []
+let chunksSize = {
+    width: 0,
+    height: 0,
 }
 
-function update(){
-  // ctx.fillStyle = "rgba(" + 0x00 + ", " + 0x55 + ", " + 0x8C + ", 0.05)";
-  ctx.fillStyle = "rgba(" + 0x00 + ", " + 0x0 + ", " + 0x0 + ", 0.1)";
-  // ctx.fillStyle = "rgba(" + Math.floor(100 ) + "," + Math.floor(0) + "," + Math.floor(100) + ", 0.5)";
-
-  ctx.fillRect(0, 0, c.width, c.height);
-
-  windowPosition.vx = (window.screenX - windowPosition.x);
-  windowPosition.vy = (window.screenY - windowPosition.y);
-
-  windowPosition.x = window.screenX;
-  windowPosition.y = window.screenY;
-
-  for(var i in balls){
-    balls[i].update();
-    balls[i].draw();
-  }
-
-  window.requestAnimationFrame(update);
+function resizeHandler(){
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
 }
 
-window.addEventListener("load", windowLoad, false);
+window.addEventListener("resize", resizeHandler)
+resizeHandler()
 
-function windowResize(){
-  c.width = window.innerWidth;
-  c.height = window.innerHeight;
+const props = {
+    balls: 500,
+
+    gravityX: 0,
+    gravityY: 0.1,
+
+    friction: 0.99,
+    bounce: 0.9,
+
+    size: 24,
+    sizeRandom: 0.6,
+
+    background: [30, 20, 100],
+    hot: [180, 0, 100],
+    cold: [255, 180, 180],
+
+    trail: 50,
+
+    maxSpeed: 3,
+    constantSpeed: false,
+
+    chunkSize: 4,
+    chunkRadius: 1,
+
+    heatDelay: 20,
 }
 
-function mouseMove(e){
-  setMousePosition(e);
+const gui = new dat.GUI()
+gui.add(props, "balls", 50, 3000).step(1).name("Balls")
+.onChange(value => {
+    let diff = value - balls.length
+
+    for(let i = 0; i < Math.abs(diff); i++){
+        if(diff < 0) removeBall(balls[balls.length - 1])
+        else addBall()
+    }
+})
+
+gui.add(props, "size", 16, 32).step(0.001).name("Size")
+gui.add(props, "sizeRandom", 0, 1).step(0.001).name("Random Size")
+
+gui.addColor(props, "background").name("Background")
+gui.add(props, "trail", 0, 100).step(0.1).name("Trail")
+gui.addColor(props, "hot").name("Hot")
+gui.addColor(props, "cold").name("Cold")
+
+gui.add(props, "gravityX", -.1, .1).step(0.01).name("Gravity X")
+gui.add(props, "gravityY", -.1, .1).step(0.01).name("Gravity Y")
+
+gui.add(props, "friction", 0.9, 1).step(0.001).name("Friction")
+gui.add(props, "bounce", 0.5, 1).step(0.1).name("Bounce")
+
+gui.add(props, "maxSpeed", 1, 7).step(0.001).name("Max Speed")
+gui.add(props, "constantSpeed").name("Constant Speed")
+
+function addBall(x = canvas.width / 2 + (0.5 - Math.random()), y = canvas.height / 2 + (0.5 - Math.random())){
+    let ball = {
+        x, y,
+        vx: 0, vy: 0,
+        random: Math.random(),
+
+        chunkIndex: 0,
+        color: 0,
+
+        radius: 1,
+        mass: 1,
+
+        bumps: 0,
+        bumpRatio: 1,
+        heat: 0,
+    }
+    balls.push(ball)
+    return ball
 }
 
-function mouseDown(e){
-  G = 0.1;
-  setMousePosition(e);
-  md = true;
+function removeBall(ball){
+    if(ball.chunkIndex < chunks.length){
+        let ballIndex = chunks[ball.chunkIndex].indexOf(ball)
+        if(ballIndex !== -1) chunks[ball.chunkIndex].splice(ballIndex, 1)
+        balls.splice(balls.indexOf(ball), 1)
+    }
 }
 
-function mouseUp(e){
-  G = 0;
-  mux = mx;
-  muy = my;
-  setMousePosition(e);
-  md = false;
+for(let i = 0; i < props.balls; i++) addBall()
+
+function updateChunks(){
+    let width = (canvas.width >> props.chunkSize) + 1
+    let height = (canvas.height >> props.chunkSize) + 1
+
+    if(chunksSize.width != width || chunksSize.height != height){
+        chunks = new Array(width * height).fill(0).map(i => [])
+        chunksSize.width = width
+        chunksSize.height = height
+    }
+
+    for(let ball of balls){
+        let cx = ball.x >> props.chunkSize
+        let cy = ball.y >> props.chunkSize
+        let chunkIndex = cx + cy * chunksSize.width
+
+        if(ball.chunkIndex == chunkIndex){
+            // Ball is in the right chunk
+        } else{
+            // Remove ball from previous chunk
+            if(ball.chunkIndex < chunks.length){
+                let ballIndex = chunks[ball.chunkIndex].indexOf(ball)
+                if(ballIndex !== -1) chunks[ball.chunkIndex].splice(ballIndex, 1)
+            }
+            // Add ball to correct chunk
+            if(chunkIndex > 0 && chunkIndex < chunks.length){
+                chunks[chunkIndex].push(ball)
+                ball.chunkIndex = chunkIndex
+            }
+        }
+    }
 }
 
-function setMousePosition(e){
-  mx = e.clientX;
-  my = e.clientY;
+updateChunks()
+
+function loop(){
+    // Update
+    updateChunks()
+    for(let ball of balls){
+        ball.radius = (props.size - (0.5 * props.size * ball.random * props.sizeRandom)) / 2
+        ball.mass = Math.PI * ball.radius * ball.radius
+        ball.bumps = 0
+    }
+
+    for(let i = 0; i < chunks.length; i++){
+        let cx = i % chunksSize.width
+        let cy = Math.floor(i / chunksSize.width)
+        let chunkBalls = []
+        for(let x = -props.chunkRadius; x <= props.chunkRadius; x++){
+            for(let y = -props.chunkRadius; y <= props.chunkRadius; y++){
+                let chunkIndex = (cx + x) + (cy + y) * chunksSize.width
+                if(chunkIndex >= 0 && chunkIndex < chunks.length){
+                    chunkBalls = chunkBalls.concat(chunks[chunkIndex])
+                }
+            }
+        }
+
+        if(chunkBalls.length > 1){
+            for(let a of chunkBalls){
+                for(let b of chunkBalls){
+                    if(a !== b){
+                        let dx = b.x - a.x
+                        let dy = b.y - a.y
+                        let dist = Math.sqrt(dx * dx + dy * dy)
+                        let diff = ((a.radius + b.radius) - dist) / dist
+                        let s1 = (1 / a.mass) / ( (1 / a.mass) + (1 / b.mass) )
+                        let s2 = 1 - s1
+
+                        if(dist < a.radius + b.radius){
+                            let ex = dx * diff
+                            let ey = dy * diff
+
+                            a.x -= ex * s1
+                            a.y -= ey * s1
+                            a.vx -= ex * s1
+                            a.vy -= ey * s1
+
+                            b.x += ex * s2
+                            b.y += ey * s2
+                            b.vx += ex * s2
+                            b.vy += ey * s2
+
+                            a.bumps++
+                            b.bumps++
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    let maxBumps = 0
+
+    for(let ball of balls){
+        if(ball.x - ball.radius < 0){
+            ball.x = ball.radius
+            ball.vx *= -props.bounce
+            ball.bumps += 5
+        }
+
+        if(ball.y - ball.radius < 0){
+            ball.y = ball.radius
+            ball.vy *= -props.bounce
+            ball.bumps += 5
+        }
+
+        if(ball.x + ball.radius > canvas.width){
+            ball.x = canvas.width - ball.radius
+            ball.vx *= -props.bounce
+            ball.bumps += 5
+        }
+
+        if(ball.y + ball.radius > canvas.height){
+            ball.y = canvas.height - ball.radius
+            ball.vy *= -props.bounce
+            ball.bumps += 5
+        }
+
+        maxBumps = Math.max(ball.bumps, maxBumps, 1)
+
+        ball.vx += props.gravityX
+        ball.vy += props.gravityY
+
+        ball.vx *= props.friction
+        ball.vy *= props.friction
+
+        let vel = Math.sqrt(ball.vx * ball.vx + ball.vy * ball.vy)
+        vel = props.constantSpeed ? props.maxSpeed : Math.min(props.maxSpeed, vel)
+
+        let angle = Math.atan2(ball.vy, ball.vx)
+        ball.vx = Math.cos(angle) * vel
+        ball.vy = Math.sin(angle) * vel
+
+        ball.x += ball.vx
+        ball.y += ball.vy
+    }
+
+    for(let ball of balls){
+        ball.bumpRatio = ball.bumps/maxBumps
+        ball.heat += (ball.bumpRatio - ball.heat) / props.heatDelay
+    }
+
+    // Draw
+    const backgroundAlpha = 0.1 * (1 - props.trail/100)
+    context.fillStyle = `rgba(${props.background[0]}, ${props.background[1]}, ${props.background[2]}, ${backgroundAlpha})`
+    context.fillRect(0, 0, canvas.width, canvas.height)
+
+    for(let ball of balls){
+        const ratio = ball.heat
+        const r = props.cold[0] + (props.hot[0] - props.cold[0]) * ratio 
+        const g = props.cold[1] + (props.hot[1] - props.cold[1]) * ratio 
+        const b = props.cold[2] + (props.hot[2] - props.cold[2]) * ratio 
+        context.fillStyle = `rgb(${r}, ${g}, ${b})`
+        context.beginPath()
+        context.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2)
+        context.fill()
+    }
+
+    requestAnimationFrame(loop)
 }
 
-function keyDown(e){
-  keyCodes[e.keyCode] = true;
-}
-
-function keyUp(e){
-  keyCodes[e.keyCode] = false;
-}
+loop()
